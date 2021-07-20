8621442a1c5a7a84e36e3aa41eb2e54a45b12e0d
crazyoptimist <crazyoptimist@mail.com>
2021-07-20 05:00:14 -0500

    add markdown engine and w3 style

---
+++ markdown/about.md
@@ -0,0 +1,41 @@
+---
+title: "I am a Software Engineer"
+---
+### Programming Languages:
+JS/TS, Python, Ruby, Go, Rust
+
+### Frameworks:
+Express.js, Nest.js, Sails.js, Flask, FastAPI, Rails, Gin-Gonic, Actix  
+React, Angular, Vue  
+
+### Methodologies:
+OOP, FP, TDD  
+
+### Tools:
+Vim, Git, Docker, Podman  
+Jira, Trello, Slack  
+Jenkins, CircleCI/GitlabCI/TravisCI  
+Kubernetes, Terraform, Ansible  
+Swagger  
+Figma  
+
+### Platforms:
+AWS, GCP, Azure, DigitalOcean  
+Linux(Debian/Fedora/Arch)  
+
+### Databases:
+MongoDB, PostgreSQL, MySQL  
+
+### Interests:
+Long run stability, Extraordinary team cultures, Lifelong learning  
+Design patterns, Clean code  
+Distributed systems, Blockchain, Applied cryptography  
+
+### PGP:
+https://keys.openpgp.org/vks/v1/by-fingerprint/412E13E6D0F0083EF322BF34C9C724D3A774A5AA  
+https://keyserver.pgp.com/vkd/DownloadKey.event?keyid=0xC9C724D3A774A5AA  
+
+### Contact Me:
+crazyoptimist@protonmail.com
+
+### Thanks for reading!
--- server.js
+++ server.js
@@ -1,16 +1,48 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const md = require('markdown-it')();
+const matter = require('gray-matter');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'views')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.render("pages/index", {
-    dataText: "Hello Darknet!"
-  })
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// public dir with static scripts and styles
+app.use(express.static('public'));
+
+// path for the ejs folder
+app.set('views', path.join(__dirname, 'markdown'));
+app.set('view engine', 'ejs');
+
+
+const readBlog = (req, res) => {
+  const file = req.params.article
+    ? matter.read(__dirname + '/markdown/' + req.params.article + '.md')
+    : matter.read(__dirname + '/markdown/' + 'about.md');
+  const content = file.content;
+  const result = md.render(content);
+
+  res.render('index', {
+    post: result,
+    title: file.data.title,
+    description: file.data.description,
+    image: file.data.image
+  });
+};
+
+const listBlogs = (req, res) => {
+  const posts = fs.readdirSync(__dirname + '/markdown').filter(file => file.endsWith('.md'));
+  res.render('list', {
+    posts: posts
+  });
+};
+
+app.get('/', readBlog);
+app.get('/list', listBlogs);
+app.get('/:article', readBlog);
+
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
